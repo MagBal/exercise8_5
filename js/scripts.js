97851cd4ec9c3b45ac713c4d658e4087ff6d2ae6
Magda Balun <magi@op.pl>
2017-11-24 14:39:23 +0100

    Add index.html and js folder and file

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+alert('Calculate triangle area');
+var a = parseInt(prompt("Enter base of triangle a : ")),
+	h = parseInt(prompt("Enter height of triangle h : ")),
+	triangleArea = (a * h)/2;
+alert('Triangle field with base a = ' + a + ' and height h = ' + h + ' is equal to: ' + triangleArea);
+
