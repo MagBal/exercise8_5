97f3cc8fd571193a47b905505220840020b3ad0c
Magda Balun <magi@op.pl>
2017-11-25 08:07:32 +0100

    Fix .js file

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,6 @@
 alert('Calculate triangle area');
 var	a = parseInt(prompt("Enter base of triangle a : ")),
 	h = parseInt(prompt("Enter height of triangle h : ")),
-	triangleArea = (a * h)/2;
+	triangleArea = a * h / 2;
 alert('Triangle field with base a = ' + a + ' and height h = ' + h + ' is equal to: ' + triangleArea);
 
